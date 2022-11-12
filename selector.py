078572bfc21cac90a4ddf016984b226db6146084
kumo <h1710169@nushigh.edu.sg>
2022-11-12 12:32:41 +0800

    attempt

--- selector.py
+++ selector.py
@@ -1,15 +1,26 @@
+from io import BytesIO
+
 import PySimpleGUI as sg
+from PIL.Image import fromarray
 
 from frame_analyzer import keyFrame
 
 sg.theme('DefaultNoMoreNagging')
 
 
+def array_to_data(array):
+    im = fromarray(array)
+    with BytesIO() as output:
+        im.save(output, format="PNG")
+        data = output.getvalue()
+    return data
+
+
 def select_images(clusters):
     select_images_tabs = [[sg.Tab(f'#{idx + 1}', [
         [sg.Text("Select Objects to Exclude")],
         [sg.Frame(f"Image {i + 1}", [
-            [sg.Image(size=(75, 75), background_color='green', key=f"img{i}")],
+            [sg.Image(size=(112, 112), background_color='green', key=f"img{i}", data=array_to_data(clusters[idx][i]))],
         ]) for i in range(5)],
         [sg.Radio("Include", f"radio-g-{idx}", key=idx), sg.Radio("Exclude", f"radio-g-{idx}", default=True)],
         [sg.Button("Prev Batch", key=lambda x=idx - 1: window['tabgroup'].Widget.select(x), disabled=idx == 0),
